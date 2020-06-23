b1b225c63fd1718294a41b2d82c37b9b0854df34
Colin Stolley <ccstolley@github.com>
2020-06-22 21:01:14 -0500

    Clean up tidy warnings and notify bad decrypt.

--- pwm.cc
+++ pwm.cc
@@ -10,12 +10,12 @@
 #include <string>
 #include <vector>
 
-bool decrypt(const char *in_filename, std::string &data);
+bool decrypt(const char *in_filename, std::string *data);
 std::string trim(const std::string &s);
 std::vector<std::string> split(const std::string &s,
                                const std::string &delimiter);
 bool find(const std::string &needle, const std::string &haystack,
-          struct ent &entry);
+          struct ent *entry);
 const char *STORE_PATH = "/home/stolley//mystuff/personal/pwm/stolley.txt.enc";
 
 struct ent {
@@ -35,9 +35,9 @@ int main(const int argc, const char *argv[]) {
     exit(1);
   }
 
-  if (decrypt(STORE_PATH, data)) {
-    if (find(argv[1], data, entry)) {
-      fprintf(stderr, "%s: %s\n", entry.name.c_str(), entry.meta.c_str());
+  if (decrypt(STORE_PATH, &data)) {
+    if (find(argv[1], data, &entry)) {
+      fprintf(stderr, "\n%s: %s\n", entry.name.c_str(), entry.meta.c_str());
       printf("%s\n", entry.password.c_str());
     } else {
       fprintf(stderr, "Not found.\n");
@@ -48,7 +48,7 @@ int main(const int argc, const char *argv[]) {
 }
 
 bool find(const std::string &needle, const std::string &haystack,
-          struct ent &entry) {
+          struct ent *entry) {
   std::stringstream linestream{haystack};
   std::stringstream costream{};
   int i = -1;
@@ -62,13 +62,13 @@ bool find(const std::string &needle, const std::string &haystack,
       fprintf(stderr, "warning: line %d missing data", i);
       continue;
     }
-    entry.name = fields[0];
+    entry->name = fields[0];
     auto data = split(fields[1], " ");
     if (data.size() == 1) {
-      entry.password = data[0];
+      entry->password = data[0];
     } else {
-      entry.meta = data[0]; // typically username
-      entry.password = data[data.size() - 1];
+      entry->meta = data[0]; // typically username
+      entry->password = data[data.size() - 1];
     }
     return true;
   }
@@ -77,7 +77,8 @@ bool find(const std::string &needle, const std::string &haystack,
 
 std::vector<std::string> split(const std::string &s,
                                const std::string &delimiter) {
-  size_t start = 0, end = 0;
+  size_t start = 0;
+  size_t end = 0;
   std::string token;
   std::vector<std::string> rv;
   while ((end = s.find(delimiter, start)) != std::string::npos) {
@@ -99,8 +100,9 @@ std::vector<std::string> split(const std::string &s,
 std::string trim(const std::string &s) {
   auto front = std::find_if_not(
       s.begin(), s.end(), [](unsigned char c) { return std::isspace(c); });
-  if (front == s.begin())
+  if (front == s.begin()) {
     return s;
+  }
   return std::string(
       front,
       std::find_if_not(s.rbegin(), std::string::const_reverse_iterator(front),
@@ -111,15 +113,17 @@ std::string trim(const std::string &s) {
 /**
  * Decrypt the contents of filename and store it in data.
  */
-bool decrypt(const char *in_filename, std::string &data) {
+bool decrypt(const char *in_filename, std::string *data) {
   static const char magic[] = "Salted__";
   char buf[255];
   char mbuf[sizeof magic - 1];
   char key[EVP_MAX_KEY_LENGTH];
-  unsigned char dkey[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
+  unsigned char dkey[EVP_MAX_KEY_LENGTH];
+  unsigned char iv[EVP_MAX_IV_LENGTH];
   unsigned char salt[PKCS5_SALT_LEN];
 
-  BIO *in = NULL, *benc = NULL;
+  BIO *in = NULL;
+  BIO *benc = NULL;
   EVP_CIPHER_CTX *ctx = NULL;
   const EVP_CIPHER *cipher = EVP_aes_256_cbc();
 
@@ -139,12 +143,13 @@ bool decrypt(const char *in_filename, std::string &data) {
   }
 
   if (BIO_read(in, mbuf, sizeof mbuf) != sizeof mbuf ||
-      BIO_read(in, (unsigned char *)salt, sizeof salt) != sizeof salt) {
+      BIO_read(in, reinterpret_cast<unsigned char *>(salt), sizeof salt) !=
+          sizeof salt) {
     BIO_printf(bio_err, "error reading input file\n");
     goto end;
   }
 
-  if (std::memcmp(mbuf, magic, sizeof magic - 1)) {
+  if (std::memcmp(mbuf, magic, sizeof magic - 1) != 0) {
     BIO_printf(bio_err, "bad magic number\n");
     goto end;
   }
@@ -154,8 +159,9 @@ bool decrypt(const char *in_filename, std::string &data) {
     goto end;
   }
 
-  if (EVP_BytesToKey(cipher, EVP_sha256(), salt, (unsigned char *)key,
-                     strlen(key), 1, dkey, iv) == 0) {
+  if (EVP_BytesToKey(cipher, EVP_sha256(), salt,
+                     reinterpret_cast<unsigned char *>(key), strlen(key), 1,
+                     dkey, iv) == 0) {
     perror("failed to derive key and iv");
     goto end;
   }
@@ -167,7 +173,7 @@ bool decrypt(const char *in_filename, std::string &data) {
 
   BIO_get_cipher_ctx(benc, &ctx);
 
-  if (!EVP_CipherInit_ex(ctx, cipher, NULL, dkey, iv, false)) {
+  if (EVP_CipherInit_ex(ctx, cipher, NULL, dkey, iv, 0) != 1) {
     perror("failed to init cipher");
     goto end;
   }
@@ -176,12 +182,15 @@ bool decrypt(const char *in_filename, std::string &data) {
 
   for (;;) {
     int inl = BIO_read(in, buf, sizeof buf);
-    if (inl <= 0) {
+    if (ERR_get_error() != 0) {
+      goto end;
+    } else if (inl <= 0) {
       break;
     }
-    data.append(buf, inl);
+    data->append(buf, inl);
   }
 
+  BIO_free_all(in);
   return true;
 
 end:
